700bc35e858557eb03a20865c4c3a737e57e9f24
gagan-a18 <94117543+gagan-a18@users.noreply.github.com>
2024-03-12 14:44:19 +0530

    Update SectionWrapper.tsx

--- src/Wrapper/SectionWrapper.tsx
+++ src/Wrapper/SectionWrapper.tsx
@@ -7,7 +7,7 @@ const SectionWrapper = (Component: ReactNode, name: string) =>
   function HOC() {
     return (
 
-      <motion.section initial="hidden" whileInView="show" viewport={{ once: true, amount: 0.25 }} className={`${styles.paddingX} max-w-7xl mx-auto relative z-0`} >
+      <motion.section initial="hidden" whileInView="show" viewport={{ once: true, amount: 0.25 }} className={`${styles.paddingX} max-w-7xl mx-auto relative z-0 mt-2`} >
         {Component}
         <span className="hash-span" id={name}> &nbsp;</span>
       </motion.section>
