20d6dd9c8300cf0322a1dae39f75ec719d9bc7ba
gagan-a18 <94117543+gagan-a18@users.noreply.github.com>
2024-03-15 11:47:19 +0530

    Update SectionWrapper.tsx

--- src/Wrapper/SectionWrapper.tsx
+++ src/Wrapper/SectionWrapper.tsx
@@ -8,8 +8,8 @@ const SectionWrapper = (Component: ReactNode, name: string) =>
     return (
       
       <motion.section initial="hidden" whileInView="show" viewport={{ once: true, amount: 0.25 }} className={`${styles.paddingX} max-w-7xl mx-auto relative`} >
-        {Component}
-        <span className="hash-span" id={name}> &nbsp;</span>
+        <span className="hash-span -z-10" id={name}> &nbsp;</span>
+        {Component} 
       </motion.section>
       
     );
